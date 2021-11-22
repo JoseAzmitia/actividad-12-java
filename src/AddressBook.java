9942c7f9b76b30f12182c2e77107e2f082fd35e5
JoseAzmitia <azmitiajc@gmail.com>
2021-11-22 02:09:15 -0600

    Se creo añadió el archivo csv y metodos nuevos

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class AddressBook {
     HashMap<String, String> agenda = new HashMap<>();
     String numero, nombre;
+
 
     public void list(){
         if (agenda.entrySet().isEmpty()){
@@ -40,11 +43,41 @@ public class AddressBook {
         }
     }
 
-    public void load(){
-
-    }
+    public void load(File filePath) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(filePath)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] lineParts = line.split("\s*,\s*");
+                numero = lineParts[0];
+                nombre = lineParts[1];
+                agenda.put(numero, nombre);
+            }
+        }
+        for (Map.Entry<String, String> entry : agenda.entrySet()) {
+            System.out.println("Número:" + entry.getKey() + " Nombre:" + entry.getValue());
+        }
+        }
 
     public void save(){
+        String eol = System.getProperty("line.separator");
 
+        try (Writer writer = new FileWriter("src/contactos.csv")) {
+            for (Map.Entry<String, String> entry : agenda.entrySet()) {
+                writer.append(entry.getKey())
+                        .append(',')
+                        .append(entry.getValue())
+                        .append(eol);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "numero='" + numero + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,40 @@
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        showMenu();
+    }
+
+    public static void showMenu() throws IOException {
+        byte option = 0;
+        AddressBook agenda = new AddressBook();
+        File myObj = new File("src/contactos.csv");
+        agenda.load(myObj);
+        do{
+            try {
+                option = Byte.parseByte(JOptionPane.showInputDialog("""
+                        Agenda de contactos
+                        Selecciona una opción
+                        1. Ver lista
+                        2. Añadir contacto
+                        3. Borrar contacto
+                        0. Salir"""));
+            }catch (Throwable e){
+                JOptionPane.showMessageDialog(null, "Opción inválida");
+                showMenu();
+            }
+            switch (option) {
+                case 1 -> agenda.list();
+                case 2 -> agenda.create();
+                case 3 -> agenda.delete();
+                case 0 -> {
+                    agenda.save();
+                    option = 0;
+                }
+                default -> JOptionPane.showMessageDialog(null, "Opción inválida");
+            }
+        }while (option != 0);
+    }
 }
